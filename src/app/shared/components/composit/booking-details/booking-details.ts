732c94be2b9e517421078da01c3bdcbcdb932ab9
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2026-02-10 14:07:56 +0600

    dfsdf

--- src/app/shared/components/composit/booking-details/booking-details.ts
+++ src/app/shared/components/composit/booking-details/booking-details.ts
@@ -13,6 +13,31 @@ export class BookingDetails {
   bookingDetails = signal<any>(null);
   isLoading = signal<boolean>(false);
 
+  details = {
+    guestName: 'Ashish Nehra',
+    reference: 'AGT416182310',
+    email: 'ashishnehra@raynatours.com',
+    phone: '+971 12 1235 2356',
+    totalPrice: 200,
+    currency: 'AED',
+    status: 'Pending',
+    yachtDetails: {
+      title: 'From Dubai Marina Sightseeing Yacht',
+      imageUrl: 'assets/yacht-demo.jpg', // Path to your yacht image
+      date: '25 Sep 2024',
+      category: 'Private Yacht',
+      time: '03:00 PM - 07:00 PM',
+      duration: '5 Hours',
+      pax: '8 Adults 4 Child 2 Infant',
+      cancellationDeadline: '05 Aug 2024 04:29',
+      confirmationNumber: 'TRA2541654',
+      user: 'Rajesh Verma',
+      provider: 'Paramount Tourism',
+      supplier: 'Ashish Nehra',
+      location: 'Al Barsha South, Dubai',
+    },
+  };
+
   // ngOnInit() {
   //   this.loadData();
   // }
@@ -28,7 +53,4 @@ export class BookingDetails {
   //     error: () => this.isLoading.set(false),
   //   });
   // }
-
-  
-
 }
